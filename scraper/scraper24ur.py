fbbd738d73738b5f49d7cedf04df6bfe745db7ef
Samo Borzatta <samo.borzatta@gmail.com>
2025-09-10 10:57:15 +0200

    Narejena scraperja za 24 ur in BBC. Lahko se jih še prilagodi

--- scraper/scraper24ur.py
+++ scraper/scraper24ur.py
@@ -42,7 +42,7 @@ def req(url):
 # Postrga osnovno stran z tujimi novicami
 def scrape_24ur_tujina():
     result = []
-    for index in range(1, 2):
+    for index in range(1, 21):
         url = f"https://www.24ur.com/arhiv/novice/tujina/?p={index}"
         soup = req(url)
         scraped_articles = []
@@ -61,6 +61,7 @@ def scrape_24ur_tujina():
         result.append(singleScrape(scraped_articles))
 
         # Izpiši zbrane podatke v JSON formatu na standardni izhod (stdout).
+        # Izpiši zbrane podatke v JSON formatu na standardni izhod (stdout).
     res = json.dumps(result, indent=4, ensure_ascii=False)
 
     print(res)
@@ -72,15 +73,9 @@ def singleScrape(scraped_articles: list):
     for link in scraped_articles:
         print(link['link'])
         soup = req(link['link'])
-        summary = soup.find('p', class_='text-article-summary').text # type: ignore
-
-        body = soup.find('div', class_='article__body') # type: ignore
-        text: list = body.find_all('p') # type: ignore
-
-        for index, item in enumerate(text):
-            text[index] = item.text
-        deli = ''
-        txt = deli.join(text) # type: ignore
+        summary = soup.find('p', class_='text-article-summary')
+        body = soup.find('div', class_='article__body').text # type: ignore
+        text: list = str(body.find_all('p')).strip('[]') # type: ignore
 
         article_data = {
             'link': link['link'], # type: ignore
