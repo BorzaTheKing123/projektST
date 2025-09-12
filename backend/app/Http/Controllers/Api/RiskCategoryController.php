f846963d699881091cd5f45708bba02e82330e33
Samo Borzatta <samo.borzatta@gmail.com>
2025-09-12 14:00:16 +0200

    Popravljen bug od Tomšiča

--- backend/app/Http/Controllers/Api/RiskCategoryController.php
+++ backend/app/Http/Controllers/Api/RiskCategoryController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\HeatmapModels\Risk;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class RiskCategoryController extends Controller
